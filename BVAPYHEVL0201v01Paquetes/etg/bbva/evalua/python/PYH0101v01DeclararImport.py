611880d89d17c2359138662a0ea818644d5028d6
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-02-22 12:40:33 -0500

    Manejo de paquetes

---
+++ BVAPYHEVL0201v01Paquetes/etg/bbva/evalua/python/PYH0101v01DeclararImport.py
@@ -0,0 +1,15 @@
+#==============================================================================
+# Entidad			:	Entelgy - Banco Continental
+# Proyecto			:	EVL (Evaluación de Python 3.6.1.)
+# Módulo			:
+# Fecha	Creación	:	07Feb2018
+# Objetivo			:	Introducción a Python
+# Fecha Edición		:
+# Descripción		:  Cada método de una clase contiene como primer parámetro
+#                       self que hace que referencie a un objeto
+#==============================================================================
+import BVAPYHEVL0201v01Paquetes.etg.bbva.evalua.python.PYH0102v01Modulo1
+from BVAPYHEVL0201v01Paquetes.etg.bbva.evalua.python.PYH0103v01Modulo2 import CEModulo2
+import io
+
+
---
+++ BVAPYHEVL0201v01Paquetes/etg/bbva/evalua/python/PYH0102v01Modulo1.py
@@ -0,0 +1,15 @@
+#==============================================================================
+# Entidad			:	Entelgy - Banco Continental
+# Proyecto			:	EVL (Evaluación de Python 3.6.1.)
+# Módulo			:
+# Fecha	Creación	:	22Feb2018
+# Objetivo			:	Paquetes en Python
+# Fecha Edición		:
+# Descripción		:   Declarar clase para su uso en import
+#==============================================================================
+class CEModulo1:
+    def __init__(self):
+        print(self)
+
+def CEModulo():
+    print("Clase CEModulo1")
