ea325d3b5bd82242446557394e7d7a1b00af953e
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-03-26 12:44:03 -0500

    Paquetes : Estructura

---
+++ BVAPYHEVL0201v01Paquetes/etg/bbva/evalua/python/PYH0103v01Modulo2.py
@@ -0,0 +1,18 @@
+#==============================================================================
+# Entidad			:	Entelgy - Banco Continental
+# Proyecto			:	EVL (Evaluación de Python 3.6.1.)
+# Módulo			:
+# Fecha	Creación	:	22Feb2018
+# Objetivo			:	Paquetes en Python
+# Fecha Edición		:
+# Descripción		:  Declarar clase para su uso en import
+#==============================================================================
+class CEModulo2:
+    def __init__(self):
+        self.descripcion = "iniciar la clase CEModulo2"
+        print(self)
+    def show(self):
+        print(self.descripcion)
+
+def CEModulo():
+    print("La clase CEModulo2")
