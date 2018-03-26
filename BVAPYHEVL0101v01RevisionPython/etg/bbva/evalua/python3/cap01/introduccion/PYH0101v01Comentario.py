5d0b4f6d3626f0d9bc72c1ee5444ebe383a8128a
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2018-03-26 12:36:22 -0500

    Introduccion Python 3.6.1

---
+++ BVAPYHEVL0101v01RevisionPython/etg/bbva/evalua/python3/cap01/introduccion/PYH0101v01Comentario.py
@@ -0,0 +1,14 @@
+#==============================================================================
+# Entidad			:	Entelgy - Banco Continental
+# Proyecto			:	EVL (Evaluación de Python 3.6.1.)
+# Módulo			:
+# Fecha	Creación	:	18Ene2018
+# Objetivo			:	Introducción a Python
+# Fecha Edición		:	
+# Descripción		:	
+#==============================================================================
+print("Prefijo de evaluación : EVL0101v01 DeclaraComentario")
+print("Prefijo de evaluación : EVL")
+print("Declaración de un comentario")
+
+
