aedc5e3ee2dab60fb1efd58053d4305dfeb14a81
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2019-02-15 16:05:19 -0500

    Estructuras de Control Números con range Formato a Numeros enteros

---
+++ BVAPYHEVL0801v01EstructuraControl/etg/bbva/evalua/python365/CV0102v01NumerosGrandes.py
@@ -0,0 +1,12 @@
+#==============================================================================
+# Entidad			    :	Entelgy - Banco Continental
+# Proyecto			    :	EVL (Evaluación de Python 3.6.5.)
+# Módulo			    :
+# Fecha	Creación	:	15Ene2018
+# Objetivo			    :	Función range
+# Fecha Edición		:
+# Descripción		:
+#==============================================================================
+print("Base Valor Exponente")
+for i in range(60):
+    print(i, 17**i)
---
+++ BVAPYHEVL0801v01EstructuraControl/etg/bbva/evalua/python365/CV0103v01NumeroEnteroComas.py
@@ -0,0 +1,33 @@
+#==============================================================================
+# Entidad			    :	Entelgy - Banco Continental
+# Proyecto			    :	EVL (Evaluación de Python 3.6.5.)
+# Módulo			    :
+# Fecha	Creación	:	15Ene2019
+# Objetivo			    :	Adicionar una coma a los importes para mejorar su visualización
+#
+# Fecha Edición		:
+# Descripción		:   Convertir a cadena, para poder tomar cada dígito.
+#                                  Recorrer la cadena de derecha a izquierda, de tres en tres dígitos
+#                                  Se inserta la coma cada vez si todavía quedan más de tres dígitos a la izquierda.
+#==============================================================================
+def ponerComa(pNumero):
+    sNumeroCadena = str(pNumero)
+    iLongitudNumero = len(sNumeroCadena)
+    TOPE_TAMANO_MILES = 3
+    SIGNO_COMA = ','
+
+    print("Número en cadena    : ", sNumeroCadena)
+    print("Longitud de Número : ", iLongitudNumero)
+    while iLongitudNumero > TOPE_TAMANO_MILES:
+        iLongitudNumero = iLongitudNumero - TOPE_TAMANO_MILES
+        print("Nueva Longitud de Número : ", iLongitudNumero)
+        print("Nuevo derecha : ", sNumeroCadena[:iLongitudNumero])
+        print("Nuev izquierda : ", sNumeroCadena[iLongitudNumero:])
+        sNumeroCadena = sNumeroCadena[:iLongitudNumero] + SIGNO_COMA + sNumeroCadena[iLongitudNumero:]
+
+    return sNumeroCadena
+
+
+print("Base Valor Exponente")
+for i in range(10):
+    print('%3d %20s' %(i, ponerComa(5**i)))
