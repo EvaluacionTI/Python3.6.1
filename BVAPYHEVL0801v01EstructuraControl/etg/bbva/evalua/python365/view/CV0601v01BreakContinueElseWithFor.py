2c5432f50da09d923ad9d6921a32edc3614c09ef
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2019-02-21 09:32:28 -0500

    Determinar si un numero es primo o no

---
+++ BVAPYHEVL0801v01EstructuraControl/etg/bbva/evalua/python365/view/CV0601v01BreakContinueElseWithFor.py
@@ -0,0 +1,19 @@
+#==============================================================================
+# Entidad			        :	Entelgy - Banco Continental
+# Proyecto			        :	EVL (Evaluación de Python 3.6.5.)
+# Módulo			        :
+# Fecha	Creación	    :	21Febrary2019
+# Objetivo			        :	Flujo Condicional
+#                                      BREAK CONTINUE ELSE
+# Fecha Edición		    :
+# Descripción		    :   Validar un número primo
+#
+#==============================================================================
+for n in range(2, 17):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'es igual a', x, '*', n/x)
+            break
+        else:
+            # sigue el bucle sin encontrar un factor
+            print(n, 'es un numero primo')
