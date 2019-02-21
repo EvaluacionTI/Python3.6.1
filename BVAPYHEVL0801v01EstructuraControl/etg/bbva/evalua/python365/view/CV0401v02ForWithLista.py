95eb39a8ca24cf9f3e1ee2126b6ebda0bca9f95c
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2019-02-21 09:22:00 -0500

    Sentencia FOR y su aplicación with Range and listas

---
+++ BVAPYHEVL0801v01EstructuraControl/etg/bbva/evalua/python365/view/CV0401v02ForWithLista.py
@@ -0,0 +1,15 @@
+#==============================================================================
+#==============================================================================
+# Entidad			        :	Entelgy - Banco Continental
+# Proyecto			        :	EVL (Evaluación de Python 3.6.5.)
+# Módulo			        :
+# Fecha	Creación	    :	20January2019
+# Objetivo			        :	Flujo Condicional
+#
+# Fecha Edición		    :
+# Descripción		    :   FOR with RANGE
+#==============================================================================
+
+palabras = ['dog', 'windows', 'defenestrado']
+for p in palabras:
+    print(p, len(p))
