9e632a4470677475cecef694a52c4e0fe45c9e1b
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2019-03-15 16:58:57 -0500

    Lectura de datos: Origen : Archivo de texto Quitar el cambio de linea Convertir a una lista De la lista quitar la coma de separación Formar unas listas para estado y capital Mostrar las lista unificada

--- BVAPYHEVL0601v01ManejoFicheros/pe/etg/bbva/evalua/view/CV0401v01LeerCapitalCiudadMexico.py
+++ BVAPYHEVL0601v01ManejoFicheros/pe/etg/bbva/evalua/view/CV0401v01LeerCapitalCiudadMexico.py
@@ -7,55 +7,69 @@
 # Descripción		    :   Lectura de las capitales de la Ciudad de Mexico.
 #==============================================================================
 
+archivo_texto = 'CV0401v01CapitalCiudadMexico.txt'
+list_capitales = []
+list_estados = []
+list_unicos = []
 
-oarchivo = open("capitalCiudadMexico.txt")
-print ("Open Archivo : ", oarchivo)
 
-odatos = oarchivo.read()
-print("Leer Datos : ", odatos)
+def abrir_archivo_texto(parchivo):
+    oarchivo = open(parchivo)
+    print ("1. Open Archivo : ", oarchivo)
+    return oarchivo
 
-# Partimos la cadena a una lista cuyos elementos son
-# los registros de nuestra 'base de datos'
-lista = odatos.split()
-print("Convertir a Lista : ", lista)
 
-# Convertimos cada elemento de la lista a una pareja,
-# usamos una función lambda:
+def leer_datos(poarchivo):
+    odatos = poarchivo.read()
+    print("2. Lectura y Datos : \n", odatos )
+    return odatos
 
-pares_clave_valor = map(lambda e: odatos.split(e, ','), lista)
-#nuevalista = list(pares_clave_valor)
-print("Pares clave valor: ", pares_clave_valor)
-#print("Pares clave valor: ", nuevalista)
 
-# Podemos crear una lista para cada campo:
-# una lista de capitales y otra lista de estados.
-# Esta es una forma de obtenerlas:
-capitales = []
-estados = []
+def quitar_coma(pCaracter):
+    return pCaracter.split(',')
 
-#for capital in pares_clave_valor:
-#    print(capital)
-#    for it in capital:
-#        print(capital)
-##    capitales.append(capital)
-#    estados.append(estado)
 
-#print("Capitales : ", capitales)
-#print("Estadio    :  ", estado)
+def nombre_unico_de_listas():
+    for nombre in list_capitales + list_estados:
+        if nombre not in list_unicos:
+            list_unicos.append(nombre)
+        else:
+            print("Nombre igual : ", nombre)
 
-# Cómo determinar los nombres únicos de ambas listas?
-# Usamos la concatenación de listas.
+    print(".-" * 80)
+    print("Lista de unicos   : ", list_unicos)
+    print("Cantidad unicos : ", list_unicos.__len__())
 
-#unicos = []
-#for nombre in capitales + estados:
-#    if nombre not in unicos:
-#        unicos.append()
- #   else:
-#        print("Nombre : ", nombre)
 
-# cuantos elementos tenemos
-#print("Numero elementos : ", len(unicos))
+def principal():
+    oarchivo = abrir_archivo_texto(archivo_texto)
+    odata = leer_datos(oarchivo)
+    print(".-"*80)
 
-# Los ordenamos alfabeticamente
-#unicos.sort()
-#print("Ordenados alfabéticamente : ", unicos)
+    print("3. Convertimos los datos a lista identificando el separador \n")
+    print("Quitar el cambio de linea")
+    lista_data = odata.split('\n')
+    print("Lista de datos : ", lista_data)
+    print(".-"*80)
+
+    print("4. Convertimos cada elemento de la lista a una pareja, usamos una función lambda: ")
+    # pares_clave_valor = map(lambda e: odata.split(e, ','), lista_data)
+    pares_clave_valor = map(quitar_coma, lista_data)
+    list_final_map = list(pares_clave_valor)
+
+    print("Map iterable : ", pares_clave_valor)
+    print("Lista iterable : ", list_final_map)
+    print(".-"*80)
+
+    print("5. Lista de capitales y estado")
+    print("Lista final de map : ", list_final_map)
+    for capital, estado in list_final_map:
+          list_capitales.append(capital)
+          list_estados.append(estado)
+    print("Lista de capitales : ", list_capitales)
+    print("Lista de estados    : ", list_estados)
+    print(".-" * 80)
+    nombre_unico_de_listas()
+
+
+principal()
