65df196949ef488c564423bfec08286c5ec737e6
alex.diaz.vasquez.contractor <alex.diaz.vasquez.contractor@bbva.com>
2019-04-01 19:23:33 -0500

    Interface grafica y captura de datos de entrada con CheckButton

--- BVAPYHEVL0201v01Paquetes/pe/etg/bbva/evalua/view/CV0104v01LeerTxtWithClassAndHtml.py
+++ BVAPYHEVL0201v01Paquetes/pe/etg/bbva/evalua/view/CV0104v01LeerTxtWithClassAndHtml.py
@@ -11,12 +11,17 @@
 # Declarar python en las variables de entorno si este no se encuentra habilitado
 # La clase buffer crea un atributo text en cada instancia que es el texto HTML que se va formando. El método add() se usa para agregar texto a la instancia, mientras que el método pop() es para regresar y vaciar el atributo de texto, como cuando lo vamos a escribir a un archivo.
 #==============================================================================
-import BVAPYHEVL0201v01Paquetes .pe.etg.bbva.evalua.view.CC0104v01LeerTxtWithClassAndHtml as html
+import BVAPYHEVL0201v01Paquetes.pe.etg.bbva.evalua.controller.CC0201v01ClassBufferHtml as oHtml
 
-# Crear dos instancias de la clase buffer:
+# 1. Crear dos instancias de la clase buffer:
 
-pagina1 = html.buffer()
-pagina2 = html.buffer()
+pagina1 = oHtml.CCBuffer()
+pagina1.add("Muestra datos")
+print("Objeto : ", pagina1)
+pagina1.add("Arquitectyra ")
+print("Objeto : ", pagina1.texto)
+
+pagina2 = oHtml.CCBuffer()
 
 # Llenar cara buffer:
 
@@ -25,14 +30,14 @@ pagina1.encabezado( '2', 'Esta es página 1')
 pagina1.liga( 'pag2.html', 'Ir a página 2' )
 pagina1.final()
 
-pagina2.inicio( 'Página 2' )
-pagina2.encabezado( '2', 'Esta es Página 2')
-pagina2.liga( 'pag1.html', 'Ir a página 1' )
-pagina2.final()
+#pagina2.inicio( 'Página 2' )
+#pagina2.encabezado( '2', 'Esta es Página 2')
+#pagina2.liga( 'pag1.html', 'Ir a página 1' )
+#pagina2.final()
 
 # Vaciar cada buffer a un archivo:
 
-fp = open('pag1.html', 'w')
+fp = open('CV0104v01GenerarPaginaHtml.html', 'w')
 fp.write(pagina1.pop())
-fp = open('pag2.html', 'w')
-fp.write(pagina2.pop())
+#fp = open('pag2.html', 'w')
+#fp.write(pagina2.pop())
