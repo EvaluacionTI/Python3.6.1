966a21e76653a7d5cec5dea7ddc21e9bf3facefb
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2019-04-03 08:27:02 -0500

    Control Radio Button, capturando la selección

---
+++ BVAPYHEVL1001v01Bibliotecas/pe/etg/bbva/evalua/view/interfacegrafica/CV0903v01ObtenerValorRadioButton.py
@@ -0,0 +1,57 @@
+#==============================================================================
+# Entidad			        :   Entelgy / Banco Continental
+# Proyecto			        :	EVL (Evaluación de Python 3.6.1.)
+# Módulo			        :   Generacion HTML
+# Fecha	Creación	    :	02Abr2019
+# Objetivo			        :   Biblioteca Tkinter
+#                                  :
+# Procedimiento       :   RadioButton
+#
+# Declaramos una variable llamada oWindowRoot, del tipo Tk().
+#
+# Además de Tk(), podemos declarar más variables
+#
+# Mainloop es el encargado de manejar y manipular todos los eventos que ocurran durante el codigo.
+#==============================================================================
+import tkinter as oFormat
+from tkinter.ttk import Radiobutton as oFormatRadioButton
+
+# 1. Declarar la ventana padre
+oWindowRoot = oFormat.Tk()
+
+oWindowRoot.title(".....[ Módulo de Evaluación ].....")
+oWindowRoot.config(bg="pink")       # Color de fondo de la ventana
+oWindowRoot.geometry("800x600") # Tamaño de la ventana 800 pixel de ancho y 600 de alto
+
+# 2. Declarar Etiquetas
+oLabel = oFormat.Label(oWindowRoot, text="Arquitectura APX / HOST ", font=("Arial Bold", 20), bg="green", fg="blue")
+oLabel.grid(column=0, row=0)
+
+oLabelTipoCanal = oFormat.Label(oWindowRoot, text="Tipo de canal :  ", font=("Verdana", 12))
+oLabelTipoCanal.grid(column=0, row=1)
+
+# 4. Seleccion
+selected = oFormat.IntVar()
+
+def clicked():
+    print("Se ha pulsado APX")
+    print(selected.get())
+
+
+def clickedHost():
+    print("Se ha pulsado HOST")
+
+
+# 3. Declarar el Radio Button
+# 3.1 Declara un estado
+oRadioCanalAPX = oFormatRadioButton(oWindowRoot, text="APX", value=1, variable=selected)
+oRadioCanalAPX.grid(column=1, row=1)
+
+oRadioCanalHost = oFormatRadioButton(oWindowRoot, text="HOST", value=2, variable=selected)
+oRadioCanalHost.grid(column=1, row=2)
+
+oButtonSeleccion = oFormat.Button(oWindowRoot, text="Pulsar ", command=clicked)
+oButtonSeleccion.grid(column=1, row=3)
+
+
+oWindowRoot.mainloop()      # Evento que llama al inicio de nuestro programa
