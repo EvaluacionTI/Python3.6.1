79d55ec185bbd089dd365f1dadb73b9cfb73675c
Alex Luis Díaz Vásquez <aldv152027@yahoo.es>
2019-04-12 13:47:49 -0500

    Version del módulo Tkinter

---
+++ BVAPYHEVL1001v01Bibliotecas/pe/etg/bbva/evalua/view/CV0102v01VersionTkinter.py
@@ -0,0 +1,14 @@
+#==============================================================================
+# Entidad			        :   Entelgy / Banco Continental
+# Proyecto			        :	EVL (Evaluación de Python 3.6.1.)
+# Módulo			        :   Generacion HTML
+# Fecha	Creación	    :	12Abr2019
+# Objetivo			        :   Biblioteca Tkinter
+#                                  :
+# Procedimiento       :   Versión de Tkinter en tu sistema
+#
+#
+#==============================================================================
+import tkinter as oFormat
+sVersionTk = oFormat.Tcl().eval('info patchlevel')
+print("Versión Tkinter :", sVersionTk)
